d854e72cc44ce25fc0622faadb10e5a4d6a66fed
Star Dream <143779704+Stardreama@users.noreply.github.com>
2025-04-06 19:51:53 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 
 // 配置数据库连接
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',     // 请替换为您的 MySQL 用户名
-  password: '20050127a', // 请替换为您的 MySQL 密码
-  database: 'github_trending',     // 数据库名称
+  host: '',
+  user: '',     // 请替换为您的 MySQL 用户名
+  password: '', // 请替换为您的 MySQL 密码
+  database: '',     // 数据库名称
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
